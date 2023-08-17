09dd0ff991fdcb96106ea0f3c67f22eb17bf187b
ErdemGKSL <zahrakon06@gmail.com>
2023-08-17 04:10:17 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Client } from "./class/Client";
 export { Order } from "./class/Order";
+export { OrderCollection } from "./class/OrderCollection";
+export { OrderManager } from "./class/OrderManager";
+export { CallbackManager } from "./class/CallbackManager";
